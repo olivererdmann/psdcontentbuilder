78b16af45e7667f5198017c75290533f6a6a4abe
olivererdmann <o.erdmann@finaldream.de>
2014-04-12 13:58:50 +0200

    PSDEZONE-1484: Extended psdNodeBuilderNodeInfo-Class for use with mulitple languages.

--- classes/contentbuilder/psdnodebuildernodeinfo.php
+++ classes/contentbuilder/psdnodebuildernodeinfo.php
@@ -45,6 +45,8 @@ class psdNodeBuilderNodeInfo {
     public $postPublishFields = [];
 
     /**
+     * Options are defined per object and not translated.
+     *
      * @var SQLIContentOptions
      */
     public $options;
@@ -56,4 +58,71 @@ class psdNodeBuilderNodeInfo {
      * @var array
      */
     public $children = [];
+
+    public $availableLanguages = [];
+
+
+
+    public function __construct(array $languages = array())
+    {
+
+        if (empty($languages)) {
+            $languages = eZContentLanguage::prioritizedLanguageCodes();
+        }
+
+        foreach ($languages as $lang) {
+            $this->postPublishFields[$lang] = [];
+            $this->fields[$lang]            = [];
+            $this->customFields[$lang]      = [];
+        }
+
+    }
+
+    public function addFieldWithLanguage($key, $value, $language = '')
+    {
+
+        $language = $this->addLanguage($language);
+
+        $this->fields[$language][$key] = $value;
+
+    }
+
+    public function addCustomFieldWithLanguage($key, $value, $language = '')
+    {
+
+        $language = $this->addLanguage($language);
+
+        $this->fields[$language][$key] = $value;
+
+    }
+
+    public function setPostPublishFieldsWithLanguage($fields, $language = '')
+    {
+
+        $language = $this->addLanguage($language);
+
+        // PostPublish can be an array for multiple values or a string for a single value.
+        if (is_array($fields)) {
+            $this->postPublishFields[$language] = $fields;
+        } else {
+            $this->postPublishFields[$language] = array($fields);
+        }
+
+    }
+
+    protected function addLanguage($language)
+    {
+
+        if (empty($language)) {
+            $language = eZLocale::currentLocaleCode();
+        }
+
+        if (!in_array($language, $this->availableLanguages)) {
+            $this->availableLanguages[] = $language;
+        }
+
+        return $language;
+
+    }
+
 }
